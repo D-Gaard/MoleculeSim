a50954075619ef25d100442f8dd2dbd63cb90a65
TheRiisen <emilruff@gmail.com>
2023-12-26 17:58:30 +0100

    Attempted code trying to threshold steps performed by molecules
    Also beginning of more advanced universe class

--- ownSim/molecules.py
+++ ownSim/molecules.py
@@ -116,6 +116,45 @@ def simple_step(universe,molecule):
   return step_taken
 
 
+#attemt 1 step, return if successful or not, and update the univrese
+#assumes energies are calculated as B*U, so the beta used is 1
+def threshold_step(universe,molecule):
+  step_taken = False
+  
+  e_current = get_energy(molecule, universe)
+
+  delta_pos = np.random.uniform(-MAX_TRANSLATE,MAX_TRANSLATE,3)
+
+  if within_box(molecule, delta_pos, universe.box_size): #check that move is possible in universe
+    mol_temp= copy.deepcopy(molecule)
+    mol_temp.move(delta_pos)
+    #pos = molecule.pos + delta_pos # compare this to all
+    
+    idx = universe.molecules.index(molecule)
+    nbs = [m for m in universe.molecules]
+    del nbs[idx]
+    gap = np.array([inter_dist(mol_temp,m) for m in nbs])
+    if any(x < 0 for x in gap): # check if we post move are trying to move inside another molecule
+      idx = gap.index(max(gap)) # get molecule idx with biggest overlap
+      counter_dist = mol_temp.radius + nbs[idx].radius - dist(nbs[idx],mol_temp) #to make them overlap less
+      unit_vec = (mol_temp.pos - nbs[idx].pos) / np.linalg.norm(mol_temp.pos - nbs[idx].pos)
+      delta_pos += counter_dist * unit_vec
+
+    mol_copy = copy.deepcopy(molecule)
+    mol_copy.move(delta_pos)
+
+    #calculate energy for potential new location
+    e_new = get_energy(molecule, universe, mol_copy)
+
+    # if move is accepted, make the real molecule perform the step
+    accepted = fc.accept_move(e_current,e_new, BETA)
+    if accepted:
+      molecule.move(delta_pos)
+      step_taken = True 
+
+  return step_taken
+
+
 #uniformly attempt to spawn spheres, if overlap, then a new position is chosen (so a mc scheeme)
 #fails after max_attempts fails for a single molecule
 def spawn_uniformly_random(num_spheres, box_dimensions, radii, max_attempts = 1000):
@@ -229,9 +268,104 @@ class SimpleUniverse:
 
       step_bool = simple_step(self,mol)
       self.update_move_stats(step_bool)
+  
+  #take a step with thresholding
+  def make_step_thresh(self):
+    if (len(self.molecules)<= 0): #check if step possible
+      print("No molecules in universe, cant perform a step")
+      
+    else: #perform a step
+      mol = self.select_molecule()
+
+      step_bool = threshold_step(self,mol)
+      self.update_move_stats(step_bool)
 
 
+class Group:
+  def __init__(self,molecules, id = 0):
+    self.molecules = molecules
+    self.id = id
+    self.total_mass = np.sum([((4/3)*np.pi*m.radius**3) for m in molecules])
 
+  def update_mass(self,molecule):
+    self.total_mass += (4/3)*np.pi*molecule.radius**3
+
+  def get_weight(self,molecule):
+    return ((4/3)*np.pi*molecule.radius**3) / self.total_mass
+
+#creates initial groups using a list of ids for each molecule
+def init_groups(molecules, id_list):
+  grps = [Group(m,i) for m,i in zip(molecules,id_list)]
+  return grps
+
+
+class GroupUniverse:
+  def __init__(self,box_size, num_molecules,radii,seed,own_molecules=None, force_fun = fc.total_force_molecule):
+    #Reproduction setup
+    self.seed = seed
+    random.seed(seed)
+    
+    #molecule/world setup
+    self.box_size = box_size
+    self.num_molecules = num_molecules
+    #define the molecular groupings of the universe
+    self.molecules = create_initial_molecules(box_size,num_molecules, radii,own_molecules) 
+    self.groups = init_groups(self.molecules, np.arange(num_molecules)) #attach a grp ID to every molecule at the beginning
+    #define force use
+    self.force_fun = force_fun
+
+
+    #stats:
+    self.move_ctr = 0 #amount of steps taken
+    #coutners for acceptence/rejection, and list of actions
+    self.acceptance_list = []
+    self.acceptance_ctr = 0
+    self.rejction_ctr = 0 
+
+
+  #select random molecule
+  def select_molecule(self):
+    return random.choice(self.molecules)
+  
+  #select random Group
+  def select_molecule(self):
+    return random.choice(self.groups)
+
+  #get state of universe
+  # 0 = position (default)
+  # 1 = with radius
+  def get_state(self,with_radius = False):
+    if (with_radius):
+      return np.array([mol.pos for mol in self.molecules]), np.array([mol.radius for mol in self.molecules])
+    else:
+      return np.array([mol.pos for mol in self.molecules])
+  
+  #get stats
+  # 0 (default) == everything
+  def get_stats(self, state=0):
+    if (state==0):
+      return  self.acceptance_ctr, self.rejction_ctr, self.acceptance_list, self.move_ctr
+  
+  #update move stats, should be caled after/at the end of move
+  def update_move_stats(self, move_bool):
+    if move_bool:
+      self.acceptance_ctr += 1
+    else:
+      self.rejction_ctr += 1
+    
+    self.move_ctr += 1
+    self.acceptance_list.append(move_bool)
+
+  #take a step (maybe add support for passing a step function)
+  def make_step(self):
+    if (len(self.molecules)<= 0): #check if step possible
+      print("No molecules in universe, cant perform a step")
+      
+    else: #perform a step
+      mol = self.select_molecule()
+
+      step_bool = simple_step(self,mol)
+      self.update_move_stats(step_bool)
     
 
 
